d7ce1adb9e0be45c330a8d56b3c246f125cd25fa
Ulukbek <orozgozhoev.u@gmail.com>
2022-08-09 17:21:17 +0600

    new folders

---
+++ Dias-Team/js/question10.js
@@ -0,0 +1,12 @@
+const zpInput = document.getElementById('task10-input-zp');
+const goalInput = document.getElementById('task10-input-goal');
+const task10Res = document.getElementById('task10-result');
+const task10 = () => {
+  const percent = 20;
+  const zp = Number(zpInput.value);
+  const goal = +goalInput.value;
+  const zpPart = zp * percent / 100;
+  const month = Math.ceil(goal / zpPart)
+
+  task10Res.innerHTML = `ВАМ ПОТРЕБУЕТСЯ: ${month} Месяцев`
+}
